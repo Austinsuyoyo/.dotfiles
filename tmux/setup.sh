ea42bda359725888cb12661cf19082817c24f2ed
austin.su <austin.su@insyde.com>
2023-01-13 18:19:45 +0800

    [tmux] add symbol on status bar

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-BASEDIR=$(dirname "$0")
+BASEDIR=$(cd "$(dirname "$1")" && pwd)/$(basename "$1")
 
 ln -sf $BASEDIR/.tmux.conf ~/.tmux.conf
 ln -sf $BASEDIR/.tmux.conf.local ~/.tmux.conf.local
