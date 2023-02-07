a1323f5a8047268ffb2c0fdaa714e7e73a580b57
austin.su <austin.su@insyde.com>
2023-02-07 14:51:35 +0800

    update tmux

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-BASEDIR=$(cd "$(dirname "$1")" && pwd)/$(basename "$1")
+BASEDIR=$(cd "$(dirname "$0")" && pwd)
 
 ln -sf $BASEDIR/.tmux.conf ~/.tmux.conf
 ln -sf $BASEDIR/.tmux.conf.local ~/.tmux.conf.local
